793668913150eb837a5b6f07bddae88c71fdbaa4
Samuel Rocha <samuelandres_rocha@unitec.edu>
2023-11-24 09:41:00 -0600

    Cambios en la conversión de bytes a int y lectura del root directory

--- include/FileSystem.h
+++ include/FileSystem.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <cstdint>
 #include <cstdio>
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -22,10 +24,8 @@ struct BootSector {
   byte sectorsPerFat[2];
   byte sectorsPerTrack[2];
   byte heads[2];
-  byte hiddenSectors[3];
+  byte hiddenSectors[4];
   byte largeSectorCount[4];
-
-  // Remainder of boot sector
 
   byte driveNumber;
   byte _notUsed;
@@ -33,31 +33,6 @@ struct BootSector {
   byte serialNumber[4];
   byte volumeLabel[11];
   byte systemId[8];
-
-  void print() {
-    printf("\n--- Boot Sector ---\n");
-    printf("JumpInstruction: %u\n", bootJumpIns);
-    printf("OEM Name: %s\n", oemName);
-    printf("Bytes per Sector: %u\n", bytesPerSector);
-    printf("Sectors per Cluster: %u\n", sectorsPerCluster);
-    printf("Reserved Area: %u\n", reservedAreaSectors);
-    printf("Fat Count: %u\n", fatCount);
-    printf("Max root entries: %u\n", rootEntriesMax);
-    printf("Total Sectors: %u\n", totalSectors);
-    printf("Media Type: %u\n", mediaType);
-    printf("Sectors per Fat: %u\n", sectorsPerFat);
-    printf("Sectors Per Track: %u\n", sectorsPerTrack);
-    printf("Heads: %u\n", heads);
-    printf("Hidden Sectors: %u\n", hiddenSectors);
-    printf("Large Sector Count: %u\n", largeSectorCount);
-    printf("Drive Number: %u\n", driveNumber);
-    printf("Not used: %u\n", _notUsed);
-    printf("Signature: %#08x\n", signature);
-    printf("Serial Number: %#08x\n", serialNumber);
-    printf("Volume Label: %s\n", volumeLabel);
-    printf("FS Type: %s\n", systemId);
-    printf("--- Boot Sector ---\n");
-  }
 };
 
 struct DirectoryEntry {
@@ -72,22 +47,26 @@ struct DirectoryEntry {
   byte modifiedTime[2];
   byte modifiedDate[2];
   byte clusterLow[2];
-  byte size[3];
+  byte size[4];
 };
 
 class FileSystem {
 
 private:
   BootSector bootSector;
+  DirectoryEntry *rootDirectory;
   ifstream *file;
-  char *fat;
+  byte *fat;
 
 public:
   FileSystem(string file);
   ~FileSystem();
   void readBootSector();
   void readFat();
-  bool readSectors(int lba, int count, char *buffer);
-  unsigned int bytesToInt(byte *bytes);
+  void readRootDir();
+  bool readSectors(int lba, int count, void *buffer);
+  unsigned int bytesToInt(byte *bytes, size_t size);
+  unsigned int bytes16ToInt(byte *bytes);
+  unsigned int bytes32ToInt(byte *bytes);
   void print();
 };
--- src/fileSystem.cpp
+++ src/fileSystem.cpp
@@ -9,16 +9,14 @@ FileSystem::FileSystem(string filename) {
   }
   readBootSector();
   readFat();
-
-  // for (int i = 0; i < 100; i++) {
-  //   printf("%04x\n", fat[i]);
-  // }
+  readRootDir();
 }
 
 FileSystem::~FileSystem() {
   file->close();
   delete file;
   delete[] fat;
+  delete[] rootDirectory;
 }
 
 void FileSystem::readBootSector() {
@@ -32,25 +30,80 @@ void FileSystem::readBootSector() {
 }
 
 void FileSystem::readFat() {
-  unsigned int length = bytesToInt(bootSector.sectorsPerFat) *
-                        bytesToInt(bootSector.bytesPerSector);
-  fat = new char[length];
-  readSectors(bytesToInt(bootSector.reservedAreaSectors),
-              bytesToInt(bootSector.sectorsPerFat), fat);
+  unsigned int length = bytes16ToInt(bootSector.sectorsPerFat) *
+                        bytes16ToInt(bootSector.bytesPerSector);
+  fat = new byte[length];
+  readSectors(bytes16ToInt(bootSector.reservedAreaSectors),
+              bytes16ToInt(bootSector.sectorsPerFat), fat);
 }
 
-bool FileSystem::readSectors(int lba, int count, char *buffer) {
-  file->seekg(lba * bytesToInt(bootSector.bytesPerSector), file->beg);
+void FileSystem::readRootDir() {
+  unsigned int lba =
+      bytes16ToInt(bootSector.reservedAreaSectors) +
+      bytes16ToInt(bootSector.sectorsPerFat) * bootSector.fatCount;
+  unsigned int size =
+      sizeof(DirectoryEntry) * bytes16ToInt(bootSector.rootEntriesMax);
+  unsigned int sectors = (size / bytes16ToInt(bootSector.bytesPerSector));
+  if (size % bytes16ToInt(bootSector.bytesPerSector) > 0) {
+    sectors++;
+  }
+  rootDirectory =
+      new DirectoryEntry[sectors * bytes16ToInt(bootSector.bytesPerSector)];
+  readSectors(lba, sectors, rootDirectory);
+}
 
-  file->read(buffer, count);
+bool FileSystem::readSectors(int lba, int sectors, void *buffer) {
+  file->seekg(lba * bytes16ToInt(bootSector.bytesPerSector), file->beg);
+
+  unsigned int bytesToRead = sectors * bytes16ToInt(bootSector.bytesPerSector);
+  printf("Reading %u bytes...\n", bytesToRead);
+  file->read(reinterpret_cast<char *>(buffer), bytesToRead);
+
+  if (!file) {
+    std::cerr << "Error al leer desde el archivo" << std::endl;
+    file->close();
+    exit(1);
+  }
 
   return true;
 }
 
-unsigned int FileSystem::bytesToInt(byte *bytes) {
-  unsigned int value = *reinterpret_cast<unsigned int *>(bytes);
-
-  return value;
+unsigned int FileSystem::bytesToInt(byte *bytes, size_t size) {
+  unsigned int resultado = 0;
+  std::memcpy(&resultado, bytes, size);
+  return resultado;
 }
 
-void FileSystem::print() { bootSector.print(); }
+unsigned int FileSystem::bytes16ToInt(byte *bytes) {
+  return bytesToInt(bytes, 2);
+}
+
+unsigned int FileSystem::bytes32ToInt(byte *bytes) {
+  return bytesToInt(bytes, 4);
+}
+
+void FileSystem::print() {
+  printf("\n--- Boot Sector ---\n");
+  printf("JumpInstruction: %u\n", bytesToInt(bootSector.bootJumpIns, 3));
+  printf("OEM Name: %s\n", bootSector.oemName);
+  printf("Bytes per Sector: %u\n", bytesToInt(bootSector.bytesPerSector, 2));
+  printf("Sectors per Cluster: %u\n", bootSector.sectorsPerCluster);
+  printf("Reserved Area: %u\n", bytesToInt(bootSector.reservedAreaSectors, 2));
+  printf("Fat Count: %u\n", bootSector.fatCount);
+  printf("Max root entries: %u\n", bytesToInt(bootSector.rootEntriesMax, 2));
+  printf("Total Sectors: %u\n", bytesToInt(bootSector.totalSectors, 2));
+  printf("Media Type: %u\n", bootSector.mediaType);
+  printf("Sectors per Fat: %u\n", bytesToInt(bootSector.sectorsPerFat, 2));
+  printf("Sectors Per Track: %u\n", bytesToInt(bootSector.sectorsPerTrack, 2));
+  printf("Heads: %u\n", bytesToInt(bootSector.heads, 2));
+  printf("Hidden Sectors: %u\n", bytesToInt(bootSector.hiddenSectors, 4));
+  printf("Large Sector Count: %u\n",
+         bytesToInt(bootSector.largeSectorCount, 4));
+  printf("Drive Number: %u\n", bootSector.driveNumber);
+  printf("Not used: %u\n", bootSector._notUsed);
+  printf("Signature: %#08x\n", bootSector.signature);
+  printf("Serial Number: %#08x\n", bytesToInt(bootSector.serialNumber, 4));
+  printf("Volume Label: %s\n", bootSector.volumeLabel);
+  printf("FS Type: %s\n", bootSector.systemId);
+  printf("--- Boot Sector ---\n");
+}
